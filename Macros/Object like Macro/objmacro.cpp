178891ff492b8a0f208917f9fa9508fbe28788a2
itsArchitM <archit1333.be22@chitkara.edu.in>
2023-03-12 22:12:12 +0530

    Example Added

--- Macros/Object like Macro/objmacro.cpp
+++ Macros/Object like Macro/objmacro.cpp
@@ -8,7 +8,14 @@ using namespace std;
 int main()
 {
     cout << "The value of pi is " << pi << endl;
-    cout << "Today's date is " << DATE;
+    cout << "Today's date is " << DATE << endl;
+
+    int area;
+    int r = 5;
+
+    area = 2 * pi * r;
+
+    cout << area;
 
     return 0;
 }
