997c25991af6d5d96cd71e230d8b4f99fca38d5d
itsArchitM <archit1333.be22@chitkara.edu.in>
2023-03-15 20:40:20 +0530

    Reference and functions

--- Pointers/Reference/reference.cpp
+++ Pointers/Reference/reference.cpp
@@ -2,14 +2,31 @@
 
 using namespace std;
 
+void check(int* &ptr)
+{
+    cout << ptr << endl;
+
+    int* i = ptr;
+    cout << i << endl;
+
+    //int *i = ptr;
+
+
+}
+
 int main()
 {
     int *ptr = new int;
+    *ptr = 5;
 
-    cout << *ptr;
+    cout << *ptr << endl;
 
     // if ptr is printed, address will be printed
     // if *ptr is printed, it will be derefernced and value will be printed
+
+    check(ptr);
+
+    cout << *ptr;
 
     return 0; 
 }
